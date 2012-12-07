10f3bbd2653db1e236ae48980c275e9e314a44ff
Ziao <Ziao@192.168.1.101>
2012-12-06 23:46:12 -0600

    commit

--- JavaCodePractice/src/chapter1/Question1.java
+++ JavaCodePractice/src/chapter1/Question1.java
@@ -1,5 +1,15 @@
 package chapter1;
 
 public class Question1 {
+	public static void main(String[] args) {
 
+	}
+
+	public static boolean method1() {
+		return true;
+	}
+
+	public static boolean method2() {
+		return true;
+	}
 }
