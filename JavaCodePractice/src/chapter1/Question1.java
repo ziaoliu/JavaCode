c07fd88ef29ac315826c0c0d94c1110e0afe4a35
Ziao <Ziao@192.168.1.101>
2012-12-14 01:04:01 -0600

    Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?

--- JavaCodePractice/src/chapter1/Question1.java
+++ JavaCodePractice/src/chapter1/Question1.java
@@ -1,21 +1,54 @@
 package chapter1;
 
+import java.util.HashSet;
+
 public class Question1 {
+	public static String str = "hgqa98qh;48yp;";
+
 	public static void main(String[] args) {
 		System.out.println("output from method1");
-		method1();
-		System.out.println("output from method2: ok?");
+		String results_1 = (method1() == true) ? "true" : "false";
+		System.out.println(results_1);
+		System.out.println("-----------------------------");
 		method2();
+		String results_2 = (method2() == true) ? "true" : "false";
+		System.out.println(results_2);
+		System.out.println("-----------------------------");
+		method3();
+		String results_3 = (method3() == true) ? "true" : "false";
+		System.out.println(results_3);
 	}
 
 	public static boolean method1() {
-		for (int i = 0; i < 100; i++) {
-
+		HashSet<Character> set = new HashSet<Character>();
+		char[] array = str.toCharArray();
+		for (char a : array) {
+			if (set.contains((Character) a))
+				return false;
+			else
+				set.add(a);
 		}
 		return true;
 	}
 
 	public static boolean method2() {
+		for (int i = 0; i < str.length(); i++) {
+			char a = str.charAt(i);
+			for (int j = i + 1; j < str.length(); j++) {
+				char b = str.charAt(j);
+				if (a == b)
+					return false;
+			}
+		}
+		return true;
+	}
+	public static boolean method3(){
+		// Assume that the default charset is ascii
+		boolean[] charset = new boolean[256];
+		for(int i = 0; i < str.length(); i++){
+			int index = str.charAt(i);
+			if(charset[index]) return false;
+		}
 		return true;
 	}
 }
