95f9638f66e54a7f43dc6a76f7e9756a7715d46e
Ziao <Ziao@192.168.1.101>
2013-01-04 22:11:53 -0600

    commit to git

---
+++ JavaCodePractice/src/chapter1/Question6.java
@@ -0,0 +1,36 @@
+package chapter1;
+
+public class Question6 {
+	private static int[][] mat = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+	public static void main(String[] args) {
+		for (int i = 0; i < mat[0].length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+		rotate();
+		for (int i = 0; i < mat[0].length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void rotate() {
+		System.out.println("Start rotating...");
+		int N = mat.length;
+		// going through layers
+		for (int layer = 0; layer < N / 2; layer++) {
+			int first = layer;
+			int last = N - 1 - layer;
+			for (int element = layer; element < last; element++) {
+
+			}
+
+		}
+
+	}
+}
