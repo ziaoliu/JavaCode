686d57447c3ce1fc9afd563016e1f18d8f471c90
Ziao <Ziao@192.168.1.101>
2013-01-05 21:22:21 -0600

    solved Question 6 (rotate symmetric matrix)

--- JavaCodePractice/src/chapter1/Question6.java
+++ JavaCodePractice/src/chapter1/Question6.java
@@ -1,9 +1,15 @@
 package chapter1;
+
 // rotate the n*n matrix
 public class Question6 {
-	private static int[][] mat = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+	private static int[][] mat = { { 0, 1, 0, 0 }, { 0, 1, 0, 0 },
+			{ 0, 1, 0, 0 }, { 0, 1, 0, 0 } };
 
 	public static void main(String[] args) {
+		if (mat.length != mat[0].length) {
+			System.out.println("Must be symmetric matrix");
+			System.exit(0);
+		}
 		for (int i = 0; i < mat[0].length; i++) {
 			for (int j = 0; j < mat.length; j++) {
 				System.out.print(mat[i][j] + " ");
@@ -26,11 +32,23 @@ public class Question6 {
 		for (int layer = 0; layer < N / 2; layer++) {
 			int first = layer;
 			int last = N - 1 - layer;
-			for (int element = layer; element < last; element++) {
+			for (int position = first; position < last; position++) {
+				int offset = position - first;
+				// copy top position (top row)
+				int top = mat[layer][first + offset];
 
+				// copy bottom --> top (left col)
+				mat[layer][first + offset] = mat[last - offset][layer];
+
+				// copy left --> right (bottom row)
+				mat[last - offset][layer] = mat[last][last - offset];
+
+				// copy top --> bottom (right col)
+				mat[last][last - offset] = mat[first + offset][last];
+
+				// copy top --> right (top row)
+				mat[first + offset][last] = top;
 			}
-
 		}
-
 	}
 }
