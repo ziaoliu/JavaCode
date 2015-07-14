9ba090cd30400c7d3ec3b2e02ff860d9e85d4f58
Ziao <Ziao@Ziao-PC>
2015-07-13 21:24:38 -0500

    median of two sorted arrays

--- JavaCodePractice/src_ubuntu_e2/test/MedianOfTwoSortedArrays_1x.java
+++ JavaCodePractice/src_ubuntu_e2/test/MedianOfTwoSortedArrays_1x.java
@@ -14,22 +14,20 @@ public class MedianOfTwoSortedArrays_1x {
 						nums1, 0, m - 1, nums2, 0, n - 1, (m + n) / 2 + 1)) / 2.0;
 		}
 
-		private int findKth(int[] nums1, int i, int i2, int[] nums2, int j,
+		private double findKth(int[] nums1, int i, int i2, int[] nums2, int j,
 				int j2, int k) {
-			int m = i2 - i + 1;
-			int n = j2 - j + 1;
-
-			if (m > n)
+			int len1 = i2 - i + 1;
+			int len2 = j2 - j + 1;
+			if (len1 > len2)
 				return findKth(nums2, j, j2, nums1, i, i2, k);
-			if (m == 0)
+			if (len1 == 0)
 				return nums2[j + k - 1];
 			if (k == 1)
 				return Math.min(nums1[i], nums2[j]);
 
-			int a = Math.min(k / 2, m);
+			int a = Math.min(k / 2, len1);
 			int b = k - a;
 
-			// ??????????compare conditions!!!!!!!!!!!
 			if (nums1[i + a - 1] > nums2[j + b - 1])
 				return findKth(nums1, i, i + a - 1, nums2, j + b, j2, k - b);
 			else if (nums1[i + a - 1] < nums2[j + b - 1])
