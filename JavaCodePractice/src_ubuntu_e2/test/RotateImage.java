39f4a62cdc3f52d9839aa59145c7197c8f67089c
ziaoliu <ziao.simon@gmail.com>
2015-08-10 09:06:26 -0500

    rotate image

---
+++ JavaCodePractice/src_ubuntu_e2/test/RotateImage.java
@@ -0,0 +1,20 @@
+package test;
+
+public class RotateImage {
+	public class Solution {
+		public void rotate(int[][] matrix) {
+			if (matrix == null || matrix.length == 0)
+				return;
+			int n = matrix.length;
+			for (int i = 0; i < n / 2; i++) {
+				for (int j = i; j < n - 1 - i; j++) {
+					int temp = matrix[i][j];
+					matrix[i][j] = matrix[n - 1 - j][i];
+					matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+					matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+					matrix[j][n - 1 - i] = temp;
+				}
+			}
+		}// end of method
+	}
+}
